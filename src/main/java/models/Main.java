f7ef402499ec90d447cec05053ca42d836b93413
MohammedK96 <32768550+MohammedK96@users.noreply.github.com>
2020-04-20 01:58:28 +0200

    Endringer++
    Endret noen ting, satt opp scene.fxml og fxmlcontroller

--- src/main/java/models/Main.java
+++ src/main/java/models/Main.java
@@ -10,10 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/view/scene.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../scene.fxml"));
 
         Scene scene = new Scene(root);
-        scene.getStylesheets().add(getClass().getResource("/view/styles.css").toExternalForm());
 
         stage.setTitle("Stasjonær PC");
         stage.setScene(scene);
