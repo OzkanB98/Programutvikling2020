63c174877f688b9f71ef6d46ce65d359195dba18
Ozkan-Bicak <32769137+Ozkan-Bicak@users.noreply.github.com>
2020-05-08 01:04:29 +0200

    Konkret komponent klasse

--- Programutvikling-gruppe65/src/main/java/org/openjfx/models/Datamaskin.java
+++ Programutvikling-gruppe65/src/main/java/org/openjfx/models/Datamaskin.java
@@ -3,25 +3,7 @@ package org.openjfx.models;
 import org.openjfx.models.objects2.*;
 
 public class Datamaskin extends TilLagring {
-    private Hovedkort hovedkort;
-    private Kabinett kabinett;
-    private Prosessor prosessor;
-    private Lagring lagring;
-    private Ram ram;
-    private Mus mus;
-    private Skjerm skjerm;
-    private Skjermkort skjermkort;
-    private Tastatur tastatur;
 
-    public Datamaskin(Hovedkort hovedkort, Kabinett kabinett, Prosessor prosessor, Lagring lagring, Ram ram, Mus mus, Skjerm skjerm, Skjermkort skjermkort, Tastatur tastatur) {
-        this.hovedkort = hovedkort;
-        this.kabinett = kabinett;
-        this.prosessor = prosessor;
-        this.lagring = lagring;
-        this.ram = ram;
-        this.mus = mus;
-        this.skjerm = skjerm;
-        this.skjermkort = skjermkort;
-        this.tastatur = tastatur;
-    }
+    //En liste med komponenter
+    //Evt bruker
 }
--- Programutvikling-gruppe65/src/main/java/org/openjfx/models/Komponent.java
+++ Programutvikling-gruppe65/src/main/java/org/openjfx/models/Komponent.java
@@ -2,15 +2,19 @@ package org.openjfx.models;
 
 import java.io.Serializable;
 
-public abstract class Komponent extends TilLagring implements Serializable {
+public class Komponent extends TilLagring implements Serializable {
     private String merke;
     private String type;
-    private double pris;
+    private Double pris;
+    private String produktKategori;
+    private String produktDetaljer;
 
-    public Komponent(String merke, String type, double pris) {
+    public Komponent(String merke, String type, Double pris, String produktKategori, String produktDetaljer) {
         this.merke = merke;
         this.type = type;
         this.pris = pris;
+        this.produktKategori = produktKategori;
+        this.produktDetaljer = produktDetaljer;
     }
 
     public String getMerke() {
@@ -21,19 +25,35 @@ public abstract class Komponent extends TilLagring implements Serializable {
         this.merke = merke;
     }
 
-    public String getModell() {
+    public String getType() {
         return type;
     }
 
-    public void setModell(String modell) {
-        this.type = modell;
+    public void setType(String type) {
+        this.type = type;
     }
 
-    public double getPris() {
+    public Double getPris() {
         return pris;
     }
 
-    public void setPris(double pris) {
+    public void setPris(Double pris) {
         this.pris = pris;
+    }
+
+    public String getProduktKategori() {
+        return produktKategori;
+    }
+
+    public void setProduktKategori(String produktKategori) {
+        this.produktKategori = produktKategori;
+    }
+
+    public String getProduktDetaljer() {
+        return produktDetaljer;
+    }
+
+    public void setProduktDetaljer(String produktDetaljer) {
+        this.produktDetaljer = produktDetaljer;
     }
 }
