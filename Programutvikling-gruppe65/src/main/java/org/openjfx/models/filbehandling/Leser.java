cab10515caf4fa402d131bc92f0d18a38ac27cf4
Ozkan-Bicak <32769137+Ozkan-Bicak@users.noreply.github.com>
2020-05-07 23:42:41 +0200

    Update Leser.java

--- Programutvikling-gruppe65/src/main/java/org/openjfx/models/filbehandling/Leser.java
+++ Programutvikling-gruppe65/src/main/java/org/openjfx/models/filbehandling/Leser.java
@@ -1,4 +1,11 @@
 package org.openjfx.models.filbehandling;
 
+import org.openjfx.models.TilLagring;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public interface Leser {
+
+    void lesFraFil(File file, TilLagring obj) throws FileNotFoundException, Exception;
 }
