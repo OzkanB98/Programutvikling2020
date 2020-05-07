342fc7cb74ebcd56574bb928382d5596e419e17e
Ozkan-Bicak <32769137+Ozkan-Bicak@users.noreply.github.com>
2020-05-07 02:20:41 +0200

    Endret setRoot metode i App til public

--- Programutvikling-gruppe65/src/main/java/org/openjfx/controllers/StartController.java
+++ Programutvikling-gruppe65/src/main/java/org/openjfx/controllers/StartController.java
@@ -1,6 +1,12 @@
 package org.openjfx.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.openjfx.App;
 
 import java.io.IOException;
@@ -8,7 +14,19 @@ import java.io.IOException;
 public class StartController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.loadFXML("Administrator");
+    private void byttTilAdmin(ActionEvent event) throws IOException {
+        /*
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/resources/org/openjfx/Administrator.fxml"));
+        Parent root = loader.load();
+        Scene adminScene = new Scene(root);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(adminScene);
+        window.show();
+
+         */
+        App.setRoot("Administrator");
     }
 }
