76409fdab81f85928edcba1d66a5464297618c35
Ozkan-Bicak <32769137+Ozkan-Bicak@users.noreply.github.com>
2020-05-08 23:59:10 +0200

    Dialogvindu ved knappetrykk i adminmeny

--- Programutvikling-gruppe65/src/main/java/org/openjfx/controllers/AdminController.java
+++ Programutvikling-gruppe65/src/main/java/org/openjfx/controllers/AdminController.java
@@ -2,6 +2,10 @@ package org.openjfx.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.openjfx.App;
 
 import java.io.IOException;
@@ -11,5 +15,17 @@ public class AdminController {
     @FXML
     private void tilbakeKnapp(ActionEvent event) throws IOException {
         App.setRoot("startside");
+    }
+
+    @FXML
+    private void leggTilKnapp(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/org/openjfx/nyKomponent.fxml"));
+        Parent root = loader.load();
+
+        Stage stage = new Stage();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
