dffe5b3e9f95c09a51bd73abf2761218f7e2966d
MohammedK96 <32768550+MohammedK96@users.noreply.github.com>
2020-05-08 22:31:54 +0200

    Update JOBJLeser.java

--- Programutvikling-gruppe65/src/main/java/org/openjfx/models/filbehandling/JOBJLeser.java
+++ Programutvikling-gruppe65/src/main/java/org/openjfx/models/filbehandling/JOBJLeser.java
@@ -1,5 +1,22 @@
 package org.openjfx.models.filbehandling;
-/*
+
+import org.openjfx.models.TilLagring;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class JOBJLeser implements Leser {
+
+    @Override
+    public void lesFraFil(File file, TilLagring obj) throws FileNotFoundException, Exception {
+
+    }
+
+    private void tomFil(File file) throws Exception {
+        if (file.length() == 0){
+            throw new Exception("Filen er tom!");
+        }
+    }
+    
 }
- */
+
