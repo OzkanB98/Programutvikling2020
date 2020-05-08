749a37624b46a7a39e63ac7e7c0f23d2c2ea3db5
Ozkan-Bicak <32769137+Ozkan-Bicak@users.noreply.github.com>
2020-05-09 01:58:07 +0200

    Fikset dropdown valg for kategorier

--- Programutvikling-gruppe65/src/main/java/org/openjfx/controllers/NyKomponentController.java
+++ Programutvikling-gruppe65/src/main/java/org/openjfx/controllers/NyKomponentController.java
@@ -1,4 +1,23 @@
 package org.openjfx.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
+import java.util.ArrayList;
+
 public class NyKomponentController {
+
+    private ObservableList<String> kategorier = FXCollections.observableArrayList("Velg kategori", "Hovedkort", "Skjermkort", "Prosessor", "Minne", "Lagring", "Strømforsyning","Kjøling", "Kabinett", "Tastatur", "Mus", "Skjerm");
+
+
+    @FXML
+    private ChoiceBox kategori;
+
+    @FXML
+    private void initialize(){
+        kategori.setValue("Velg kategori");
+        kategori.setItems(kategorier);
+    }
 }
