881326d5fab79ce573ddac6ab2b98a536c0d9562
Ozkan-Bicak <32769137+Ozkan-Bicak@users.noreply.github.com>
2020-05-09 23:05:34 +0200

    Update Inputvalidering.java

--- Programutvikling-gruppe65/src/main/java/org/openjfx/models/Inputvalidering.java
+++ Programutvikling-gruppe65/src/main/java/org/openjfx/models/Inputvalidering.java
@@ -3,4 +3,13 @@ package org.openjfx.models;
 public class Inputvalidering {
 
 
+    public void sjekkNullInput(String input) throws NullPointerException{
+        input = input.trim();
+        if(!(input.length() > 0)){
+            throw new NullPointerException("Vennligst fyll ut alle felt");
+        }
+    }
+
+    
+
 }
