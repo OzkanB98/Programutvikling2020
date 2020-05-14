3d6c3e131f410ef86ad4bbf1cb71b6307f58dd9d
Ozkan-Bicak <32769137+Ozkan-Bicak@users.noreply.github.com>
2020-05-14 15:40:50 +0200

    Update Skriver.java

--- Programutvikling-gruppe65/src/main/java/org/openjfx/models/filbehandling/Skriver.java
+++ Programutvikling-gruppe65/src/main/java/org/openjfx/models/filbehandling/Skriver.java
@@ -3,9 +3,9 @@ package org.openjfx.models.filbehandling;
 import org.openjfx.models.TilLagring;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+
 
 public interface Skriver {
 
-    void skrivTilFil(File file, TilLagring obj) throws FileNotFoundException;
+    void skrivTilFil(File file, TilLagring obj) throws Exception;
 }
