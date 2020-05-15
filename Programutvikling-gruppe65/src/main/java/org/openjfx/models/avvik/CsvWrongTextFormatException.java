2b2a8bbc27f5e28294ec9db94fb9373b66138b73
MohammedK96 <32768550+MohammedK96@users.noreply.github.com>
2020-05-15 15:32:48 +0200

    Delete CsvWrongTextFormatException.java

--- Programutvikling-gruppe65/src/main/java/org/openjfx/models/avvik/CsvWrongTextFormatException.java
+++
@@ -1,7 +0,0 @@
-package org.openjfx.models.avvik;
-
-public class CsvWrongTextFormatException extends Exception {
-    public CsvWrongTextFormatException(int line) {
-        super("Oppdaget feil tekstformat i csv fil på rad " + line);
-    }
-}
